1ce2547a893af1f4608df7631bb8a4740e97c406
Fikri San Koktas <sankoktas@hotmail.com>
2023-09-21 20:22:46 +0300

    Update app.js

--- app.js
+++ app.js
@@ -15,17 +15,13 @@ const data = {
 
 const handleRequest = async (request) => {
   const url = new URL(request.url);
+  
   if (url.pathname === "/visits") {
-    // Increment the visit count
     data.count++;
-
-    // Render the HTML page with the visit count
     return new Response(await renderFile("visits.eta", data), responseDetails);
   } else if (url.pathname === "/meaning") {
-    // Respond with the meaning of life
     return new Response("Seeking truths beyond meaning of life, you will find 43.");
   } else {
-    // Respond with "Nothing here yet." for other paths
     return new Response("Nothing here yet.");
   }
 };
